9ba5647f85a5c2b1df721e406abee73f117cd4f5
maulidaprina <mldaprna@gmail.com>
2025-05-22 11:59:50 +0700

    Praktikum 2

--- DLL_Main12.java
+++ DLL_Main12.java
@@ -21,5 +21,29 @@ public class DLL_Main12 {
         dll.clear();
         dll.print();
         System.out.println("\nSize : " +dll.size());
+        System.out.print("\n\n");
+
+
+        dll.addLast(50);
+        dll.addLast(40);
+        dll.addLast(10);
+        dll.addLast(20);
+        dll.print();
+
+        System.out.println("\nSize : " + dll.size());
+        System.out.println("=============================================");
+        dll.removeFirst();
+        dll.print();
+        
+        System.out.println("\nSize : " + dll.size());
+        System.out.println("=============================================");
+        dll.removeLast();
+        dll.print();
+
+        System.out.println("\nSize : " + dll.size());
+        System.out.println("=============================================");
+        dll.remove(1);
+        dll.print();
+        System.out.println("\nSize : " +dll.size());
     }
 }
--- DoubleLinkedLists12.java
+++ DoubleLinkedLists12.java
@@ -40,7 +40,7 @@ public class DoubleLinkedLists12 {
         if (isEmpty()) {
             addFirst (item);
         } else if (index < 0 || index > size) {
-            throw new Exception ("Nilai indeks di luar batas");
+            throw new Exception ("\nNilai indeks di luar batas");
         } else {
             Node12 current = head;
             int i = 0;
@@ -82,6 +82,60 @@ public class DoubleLinkedLists12 {
             System.out.println("\nBerhasil Diisi"); 
         } else {
             System.out.println("Linked list kosong");
+        }
+    }
+
+    public void removeFirst() throws Exception {
+        if (isEmpty()) {
+            throw new Exception ("\nLinked list masih kosong, tidak dapat dihapus!");
+        } else if (size == 1) {
+            removeLast ();
+        } else {
+            head = head.next;
+            head.prev = null;
+            size--;
+        }
+    }
+
+    public void removeLast() throws Exception {
+        if (isEmpty()) {
+            throw new Exception("\nLinked list masih kosong, tidak dapat dihapus!");
+        } else if (head.next == null) {
+            head = null;
+            size--;
+            return;
+        }
+        Node12 current = head;
+        while (current.next.next != null) {
+            current = current.next;
+        }
+        current.next = null;
+        size--;
+    }
+
+    public void remove (int index) throws Exception {
+        if (isEmpty() || index >= size) {
+            throw new Exception ("\nNilai indeks di luar batas");
+        } else if (index == 0) {
+            removeFirst();
+        } else {
+            Node12 current = head;
+            int i = 0;
+            while (i < index) {
+                current = current.next;
+                i++;
+            }
+            if (current.next == null) {
+                current.prev.next = null;
+            } else if (current.prev == null) {
+                current = current.next;
+                current.prev = null;
+                head = current;
+            } else {
+                current.prev.next = current.next;
+                current.next.prev = current.prev;
+            }
+            size--;
         }
     }
 }
